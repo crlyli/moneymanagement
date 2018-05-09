5a4fbcf0c692e41d503bbd042317aba7968e707a
Crystal Lyliston <crystal.lyliston@yahoo.com>
2018-05-09 17:16:54 -0400

    Adding files for inital data fixture. Adding models unit test and started unit test for urls

--- management/tests/test_model.py
+++ management/tests/test_model.py
@@ -1,2 +1,72 @@
 from django.test import TestCase
 from management import models
+
+
+class IncomeTestCase(TestCase):
+    fixtures = [inital_data.json]
+
+    def setUp(self):
+        pass
+
+    def test_income_string(self):
+        """Verify __str__ returns with proper formatting. """
+        income = models.Income.objects.get(pk=1)
+        self.assertEqual(income.__str__(),)#income,type
+
+
+class MainBillsTestCase(TestCase):
+    fixtures = [inital_data.json]
+
+    def setUp(self):
+        pass
+
+    def test_mainbill_string(self):
+        """ Verifying __str__ returns with proper formatting. """
+        mainbill = models.MainBill.objects.get(pk=1)
+        self.assertEqual(mainbill.__str__(), ) #bill,amount
+
+
+class ExpendableBillsTestCase(TestCase):
+    fixtures = [inital_data.json]
+
+    def setUp(self):
+        pass
+
+    def test_expendablebill_string(self):
+        """ Verify __str__ returns with proper formating."""
+        expendablebill= models.ExpendableBill.objects.get(pk=1)
+        self.assertEqual(expendablebill.__str__(), )#bill amount
+
+
+class EssentialTestCase(TestCase):
+    fixture = [inital_data.json]
+
+    def setUp(self):
+        pass
+
+    def test_essential_string(self):
+        """Verify __str__ returns with proper formating."""
+        essential = models.Essential.objects.get(pk=1)
+        self.assertEqual(essential.__str__(),) #item amount
+
+class NonEssentialTestCase(TestCase):
+    fixtures = [inital_data.json]
+
+    def setUp(self):
+        pass
+
+    def test_nonessential_string(self):
+        """Veryify __str__ returns with proper formating."""
+        nonessential = models.NonEssential.objects.get(pk=1)
+        self.assertEqual(nonessential.__str__(), )  # item amount
+
+class RecieptbyItemTestCase(TestCase):
+    fixtures = [inital_data.json]
+
+    def setUp(self):
+        pass
+
+    def test_recieptbyitem_string(self):
+        """Veryify __str__ returns with proper formating."""
+        recieptbyitem = models.RecieptItem.objects.get(pk=1)
+        self.assertEqual(recieptbyitem.__str__(), )  # item amount
--- management/tests/test_unit.py
+++ management/tests/test_unit.py
@@ -2,3 +2,25 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 from django.contrib.auth.models import User
 from .. import views
+
+
+class PageTest(TestCase):
+    """ All pages should:
+        1. Resolve to the proper View
+        2. Return a 200 (login_required urls return 302 if not logged in)
+        3. Use the appropriate template
+        4. Contain the proper context data
+    """
+    fixtures = ['initial_data.json', ]
+
+    def test_mainpage(self):
+        """
+        Test the mainpage url and view
+        """
+        url = reverse('management:mainpage')
+        v = resolve(url)
+        self.assertEqual(v.func.__name__, views.MainView.__name__)
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'management/mainpage.html')
+
